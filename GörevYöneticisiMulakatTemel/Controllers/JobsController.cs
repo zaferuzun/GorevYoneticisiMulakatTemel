43985e587600c838c0ae2594ceb7cbd0d529bf12
uznzafer <uznzafer@hotmail.com>
2020-06-19 18:38:43 +0300

    users

---
+++ GörevYöneticisiMulakatTemel/Controllers/JobsController.cs
@@ -0,0 +1,147 @@
+﻿using GörevYöneticisiMulakatTemel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GörevYöneticisiMulakatTemel.Controllers
+{
+    public class JobsController : Controller
+    {
+        private DB_A633FB_gorevtakipEntities1 db = new DB_A633FB_gorevtakipEntities1();
+
+        public static class GlobalVar
+        {
+            static string _globalValue;
+            public static string UserId
+            {
+                get
+                {
+                    return _globalValue;
+                }
+                set
+                {
+                    _globalValue = value;
+                }
+            }
+        }
+        // GET: Jobs
+        public ActionResult Index(int? id)
+        {
+            var jobs = db.Jobs.Include(j => j.Users);
+            return View(jobs.ToList());
+        }
+
+        // GET: Jobs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "JobId,UserId,JobComment,JobType")] Jobs jobs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jobs.Add(jobs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", jobs.UserId);
+            return View(jobs);
+        }
+
+        // GET: Jobs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", jobs.UserId);
+            return View(jobs);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "JobId,UserId,JobComment,JobType")] Jobs jobs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jobs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", jobs.UserId);
+            return View(jobs);
+        }
+
+        // GET: Jobs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jobs jobs = db.Jobs.Find(id);
+            db.Jobs.Remove(jobs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GörevYöneticisiMulakatTemel/Controllers/UsersController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GörevYöneticisiMulakatTemel.Models;
+namespace GörevYöneticisiMulakatTemel.Controllers
+{
+    public class UsersController : Controller
+    {
+        private DB_A633FB_gorevtakipEntities1 db = new DB_A633FB_gorevtakipEntities1();
+        // POST: Users/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Users/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Users objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                var obj = db.Users.Where(a => a.UserName == objUser.UserName && a.UserPassword == objUser.UserPassword).FirstOrDefault();
+                if (obj != null)
+                {
+                    string r = "../Jobs/Index/" + obj.UserId.ToString();
+                    return RedirectToAction(r);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Kullanıcı adı veya şifreyi yanlış girdiniz.");
+                }
+            }
+            return View(objUser);
+        }
+
+        // GET: Users/Register
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        // POST: Users/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Users users)
+        {
+
+            // Kullanıcı adı bulunmasındaki hata
+            if (db.Users.Any(model => model.UserName == users.UserName))
+            {
+                ModelState.AddModelError("UserName", "Bu kullanıcı adı bulunmaktadır.");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(users);
+                db.SaveChanges();
+                string r = "../Users/Login";
+                return RedirectToAction(r);
+            }
+
+            return View(users);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
